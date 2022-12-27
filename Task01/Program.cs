366f972967858abb8f0f7aad2cc7f390fc31518e
Kiberlexx <kiberlexx@mail.ru>
2022-12-27 14:05:22 +0800

    add Task 02 Максимальное и минимальное из 2-х чисел

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,6 +1,11 @@
-﻿//int number = 5;
+﻿int number = 5;
+Console.WriteLine(number*number);
+
+
+//Console.WriteLine("Введите число");
+//int number = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine(number*number);
 
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number*number);
+//Console.WriteLine("Введите число");
+//int number = int.Parse(Console.ReadLine() ?? "0");
+//Console.WriteLine(number*number);
---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 12;
+int b = 9;
+int max;
+int min;
+if (a>b)
+{
+max=a;
+min=b;
+}
+else
+{
+max=b;
+min=a;
+}
+ Console.Write("Максимальное значение равно:");
+ Console.WriteLine(max);
+Console.Write("Минимальное значение равно:");
+ Console.WriteLine(min);
